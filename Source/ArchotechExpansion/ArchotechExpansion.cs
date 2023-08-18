920ce4b1351756a3a86a8080c1b5a57b6da38214
GmOcean <hyperassassin@gmail.com>
2023-08-18 03:24:36 -1000

    Initial commit

---
+++ Source/ArchotechExpansion/ArchotechExpansion.cs
@@ -0,0 +1,19 @@
+﻿// OCEArchotechExpansion, Version=0.0.1, Culture=neutral, PublicKeyToken=null
+// OCEArchotechExpansion.Building_AncientArchotechDoor
+using RimWorld;
+using Verse;
+
+namespace ArchotechExpansion
+{
+    public class Building_AncientArchotechDoor : Building_Door
+    {
+        public override bool PawnCanOpen(Pawn p)
+        {
+            if (((Building_Door)this).PawnCanOpen(p))
+            {
+                return ((ThingWithComps)this).GetComp<CompPowerTrader>().PowerOn;
+            }
+            return false;
+        }
+    }
+}
